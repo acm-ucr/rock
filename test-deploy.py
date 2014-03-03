f5936e58b20e83fcebe0455746b24a56240ce257
brownhead <jsull003@ucr.edu>
2014-03-03 15:50:59 -0800

    Deploy script now restarts when app changes.

--- test-deploy.py
+++ test-deploy.py
@@ -27,6 +27,9 @@ import os
 import mimetypes
 import wsgiref
 import wsgiref.simple_server
+import hashlib
+import time
+import multiprocessing
 
 # internal
 import signup_server.main
@@ -39,6 +42,9 @@ DEFAULT_ADDRESS = "localhost"
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(sys.argv[0]))
 """The directory the script is running within."""
+
+WSGI_DIR = os.path.join(SCRIPT_DIR, "signup_server")
+"""The directory containing the WSGI application."""
 
 STATIC_DIR = os.path.join(SCRIPT_DIR, "main_site")
 """The directory containing the static portions of the site."""
@@ -82,7 +88,7 @@ def proxy_app(environ, start_response):
         # We have to tell the user what kind of file we're giving it (text,
         # image, cupcakes). The simplest way to do this is to look at the
         # extension of the file we're about to serve and guess at the data
-        # inside using that information. The mimtypes module has a function
+        # inside using that information. The mimetypes module has a function
         # that does the guessing for us. The encoding has to do with whether
         # or not the files are compressed or otherwise transformed, we don't
         # use it.
@@ -100,6 +106,58 @@ def proxy_app(environ, start_response):
         # code handle it, so just call our main wsgi app to deal with the
         # rest.
         return signup_server.main.app(environ, start_response)
+
+def serve_site(address, port, app):
+    # Serve the application until our process is killed
+    httpd = wsgiref.simple_server.make_server(address, port, app)
+    httpd.serve_forever()
+
+def get_state_token(dir_path):
+    """
+    This function will iterate through the names and timestamps on every file
+    in the given directory and return a unique hash of that information.
+
+    If you take a token returned by this function and compare it to a token of
+    the same directories, they will only be different if a file was added or
+    removed, a file was updated, a file was renamed, or a directory was
+    added/removed/renamed.
+
+    """
+
+    hasher = hashlib.md5()
+    for root, dirs, files in os.walk(dir_path, topdown = True):
+        # Setting topdown to True above allows us to modify the directory list
+        # in dirs. The walk function will visit each item in the order in which
+        # they appear in that list. So here we sort it to make sure that the
+        # order in which they are visited is well defined.
+        dirs.sort()
+
+        # Create a list of the directory's absolute paths (this is a list
+        # comprehension below if you are unfamiliar with the construct).
+        absolute_dirs = [os.path.join(root, i) for i in dirs]
+
+        # Get a hash of that list (note that in Python you cannot take a hash
+        # of a list because it is mutable so we first convert the list to a
+        # tuple).
+        dirs_hash = hash(tuple(absolute_dirs))
+
+        # Add that hash into our final hash (after converting it to a string
+        # because it was an integer before).
+        hasher.update(str(dirs_hash))
+
+        # Iterate through all the files in sorted order (so we always visit
+        # them in the same order between different runs of this function).
+        for i in sorted(files):
+            # Get the absolute path of the file
+            cur_file_path = os.path.join(root, i)
+
+            # Add the aboslute path of the file to the final hash
+            hasher.update(cur_file_path)
+
+            # Add the last modified time of the file to the final hash
+            hasher.update(str(os.stat(cur_file_path).st_mtime))
+
+    return hasher.digest()
 
 def main():
     # Grab all of the arguments the user gave us, ignoring the first argument
@@ -126,10 +184,60 @@ def main():
 
     print "Serving site at http://{}:{}".format(address, port)
 
-    # Serve the application until our process is killed
-    httpd = wsgiref.simple_server.make_server(address, port, proxy_app)
-    httpd.serve_forever()
+    # Keep a counter of how many times we've killed the application, this makes
+    # it easier, when looking at the output, to verify that the application
+    # actually restarted.
+    dead_count = 0
+
+    while True:
+        # Get a token that represents the current state of the WSGI application
+        state_token = get_state_token(WSGI_DIR)
+
+        # Create a new process that, once created, will execute the
+        # serve_site() function above.
+        p = multiprocessing.Process(target = serve_site,
+            args = (address, port, proxy_app))
+
+        # This tells the multiprocessing module that we want it to try and
+        # terminate our child process when we, ourselves, terminate.
+        p.daemon = True
+
+        # Actually start the process
+        p.start()
+
+        if dead_count != 0:
+            print "Started succesfully"
+
+        # Loop until the WSGI application was updated or the process crashes
+        while p.is_alive() and state_token == get_state_token(WSGI_DIR):
+            # The amount of time is in seconds
+            time.sleep(2)
+
+        # Make sure the application is dead and give the user some feedback
+        if p.is_alive():
+            print "Killing the application (count = {})".format(dead_count)
+            p.terminate()
+            p.join()
+            print "Succesfully terminated the application."
+        else:
+            print "!! The application crashed !!"
+
+        dead_count += 1
+
+        print "Restarting..."
+        print
+
 
 # We make sure the script is not being imported above so we don't have to
 # have the typical if __name__ ==... magic here.
-sys.exit(main())
+try:
+    main()
+except KeyboardInterrupt:
+    # I write directly to standard output here and flush the buffer because I
+    # want to make sure that this text makes it to the screen before any
+    # cleanup actions take place, which usually results in some ugly output.
+    sys.stdout.write(
+        "\n\n[[[Exiting cleanly, ignore any error output below]]]\n\n")
+    sys.stdout.flush()
+
+    pass
